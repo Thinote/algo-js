1f2904b8eca2187ea7cc0938c6ec406c8a1bc1f9
Thinote <147376916+Thinote@users.noreply.github.com>
2023-10-13 09:21:45 +0200

    Add files via upload

---
+++ Exo9/main.js
@@ -0,0 +1,37 @@
+class Pokemon {
+    constructor(name, attack, defense, hp, luck){
+        this.name = name
+        this.attack = attack
+        this.defense = defense
+        this.hp = hp
+        this.luck = luck
+
+    }
+    isLucky(){
+        return Math.floor(Math.random() * 100) <= this.luck
+    }
+    attackPokemon(pokemon){
+        if (this.isLucky()){
+            let damages = this.attack - pokemon.defense
+            pokemon.hp -= damages
+            console.log(`${this.name} a attaqué ${pokemon.name}, perdant ${damage}`)
+        }else{
+            console.log(`${this.name} a raté son attaque`)
+        }
+    }
+}
+
+
+
+let pickachu = new Pokemon ("pickachu" , 10, 5, 30, 75)
+let evoli = new Pokemon ("evoli" , 15, 3, 30, 50)
+
+
+while (this.hp > 0) {
+    pickachu.attackPokemon(rondoudou)
+}
+
+
+
+
+

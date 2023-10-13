7a032ed7628302653367d23ce9a6ca373918af9f
Thinote <147376916+Thinote@users.noreply.github.com>
2023-10-13 09:57:36 +0200

    ajout correction

--- Exo9/main.js
+++ Exo9/main.js
@@ -28,8 +28,20 @@ let evoli = new Pokemon ("evoli" , 15, 3, 30, 50)
 
 
 while (pickachu.hp > 0 && evoli.hp > 0) {
-    pickachu.attackPokemon(rondoudou)
+  pickachu.attackPokemon(evoli);
+
+  if (evoli.hp <= 0) {
+    console.log("evoli a perdu le combat, il est K.O");
+    break;
+  }
+  evoli.attackPokemon(pickachu);
+
+  if (pickachu.hp <= 0) {
+    console.log("pickachu a perdu le combat, il est K.O");
+    break;
+  }
 }
+console.log("Le combat est terminé");
 
 
 
